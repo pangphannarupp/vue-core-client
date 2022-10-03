97ad0c8d83758a2ef333ff7ba3f10e4f2eb94022
pangphannarupp <36291331+pangphannarupp@users.noreply.github.com>
2022-10-03 17:14:10 +0700

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,31 @@
 var core = {}
 
-core.execute = function(key, param) {
+/**
+* JavascriptInterface
+* used to call function execute in Native Code
+*/
+var execute = function(key, param) {
     Core.execute(JSON.stringify({
         'key': key,
         'param': param
     }));
 }
 
+/**
+* Event Listener
+* used to listen response from Native
+*/
 core.listener = function(result) {
     console.log("Listen result from native => " + result);
 }
 
 core.showToast = function(message, duration) {
-    core.execute('TOAST_MESSAGE_PLUGIN', {
+    execute('TOAST_MESSAGE_PLUGIN', {
        'message': message,
        'duration': duration,
    });
 }
 
 core.showCamera = function() {
-    core.execute('CAMERA_PLUGIN', {});
+    execute('CAMERA_PLUGIN', {});
 }
